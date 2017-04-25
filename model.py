a2769164b19cdb664aba660d7a755d6b17717f8e
t.nishime <t.nishime@lexues.co.jp>
2017-04-25 12:18:44 +0900

    Add second model

--- model.py
+++ model.py
@@ -8,16 +8,20 @@ from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten, Lambda
 from keras.layers.convolutional import Conv2D, Cropping2D
+from keras.layers.advanced_activations import ELU
+from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import Adam
 from sklearn.utils import shuffle
+from keras.layers.normalization import BatchNormalization
 
 samples = []
 with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
+    fieldnames = next(reader)
     for line in reader:
         samples.append(line)
 
-train_samples, validation_samples = train_test_split(samples[1:21], test_size=0.2)
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -29,7 +33,6 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                #name = 'data/IMG/'+batch_sample[0].split('/')[-1]
                 name = 'data/'+batch_sample[0]
                 center_image = cv2.imread(name)
                 center_angle = float(batch_sample[3])
@@ -42,30 +45,32 @@ def generator(samples, batch_size=32):
             yield shuffle(X_train, y_train)
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=10)
-validation_generator = generator(validation_samples, batch_size=10)
+train_generator = generator(train_samples)
+validation_generator = generator(validation_samples)
 
 ch, row, col = 3, 90, 320  # Trimmed image format
 
 model = Sequential()
 # Preprocess incoming data, centered around zero with small standard deviation
+# Resized 25x80
 model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: x/127.5 - 1.))
+model.add(MaxPooling2D(pool_size=(4,4), padding='valid'))
 
-# Nvidia structure
-model.add(Conv2D(24, (5, 5), activation='relu', padding='valid'))
-model.add(Conv2D(36, (5, 5), activation='relu', padding='valid'))
-model.add(Conv2D(48, (5, 5), activation='relu', padding='valid'))
-model.add(Conv2D(64, (3, 3), activation='relu', padding='valid'))
-model.add(Conv2D(64, (3, 3), activation='relu', padding='valid'))
+model.add(Conv2D(24, (5, 5), activation='elu', padding='valid'))
+model.add(Conv2D(36, (5, 5), activation='elu', padding='valid'))
+model.add(BatchNormalization())
+model.add(Conv2D(48, (5, 5), activation='elu', padding='valid'))
+model.add(Conv2D(64, (3, 3), activation='elu', padding='valid'))
+model.add(Conv2D(64, (3, 3), activation='elu', padding='valid'))
+model.add(BatchNormalization())
 model.add(Flatten())
-model.add(Dense(100, activation='relu'))
-model.add(Dense(50, activation='relu'))
-model.add(Dense(10, activation='relu'))
+model.add(Dense(100, activation='elu'))
+model.add(Dense(50, activation='elu'))
+model.add(Dense(10, activation='elu'))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(train_generator, steps_per_epoch=len(train_samples), epochs=2, validation_data=validation_generator, validation_steps=len(validation_samples))
+history_object = model.fit_generator(train_generator, steps_per_epoch=len(train_samples), epochs=10, validation_data=validation_generator, validation_steps=len(validation_samples))
 model.save('model.h5')
 
 ### print the keys contained in the history object
